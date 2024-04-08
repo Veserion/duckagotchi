4b382420bf215778e12ae633d0a221d501df7128
Lobanov Daniil <lobanovdaniil@MacBook-Pro-Lobanov.local>
2024-04-08 09:03:14 +0300

    add back integration

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,54 @@
 import '../css/index.css'
 
-import {fromNano, toNano, TonClient} from "ton";
-import TonConnect from "@tonconnect/sdk";
-import TonWeb from "tonweb";
+import {fromNano, TonClient} from "ton";
+import {TonConnectUI} from '@tonconnect/ui'
 
-const connector = new TonConnect({manifestUrl: 'https://nikishmyaps.github.io/tamagotchi/tonconnect-manifest.json'});
+// const tonConnectUI = new TonConnectUI({
+//   manifestUrl: 'https://nikishmyaps.github.io/tamagotchi/tonconnect-manifest.json',
+//   buttonRootId: 'ton-connect'
+// });
+
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+  manifestUrl: 'https://nikishmyaps.github.io/tamagotchi/tonconnect-manifest.json',
+  buttonRootId: 'ton-connect'
+});
+
+const getStats = async () => {
+  const address = localStorage.getItem('walletAddress')
+  if(address) {
+    const data = await fetch(`http://44.223.23.181:5000/getStatus/${address}`)
+    const {lvl, hp} = JSON.parse(data)
+
+    const numberLvl = document.getElementById('lvl-number');
+    numberLvl.textContent = lvl;
+    const numberHp = document.getElementById('hp-number');
+    numberHp.textContent = hp;
+  }
+}
+
+await getStats()
+const putStats = async () => {
+  const address = localStorage.getItem('walletAddress')
+  if(address) {
+    const numberLvl = document.getElementById('lvl-number');
+    const lvl = parseInt(numberLvl.textContent);
+    const numberHp = document.getElementById('hp-number');
+    let hp = parseInt(numberHp.textContent);
+    const response = await fetch(`http://44.223.23.181:5000/getStatus/${address}`, {
+      method: "POST",
+      // mode: "cors", // no-cors, *cors, same-origin
+      // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      // redirect: "follow", // manual, *follow, error
+      // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({id: address, lvl, hp}), // body data type must match "Content-Type" header
+    });
+  }
+}
 
 document.querySelector('.pat').addEventListener('click', () => {
   document.querySelectorAll('.video-pat').forEach(element => {
@@ -59,19 +103,21 @@ playButton2.addEventListener('click', () => {
 
 const numberHp = document.getElementById('hp-number');
 
-setInterval(() => {
+setInterval(async () => {
   let number = parseInt(numberHp.textContent);
   number -= 2;
   numberHp.textContent = number;
+  await putStats()
 }, 30000); // 30 секунд
 
 const numberLvl = document.getElementById('lvl-number');
 
-setInterval(() => {
+setInterval(async () => {
   let number = parseInt(numberLvl.textContent);
   number++;
   numberLvl.textContent = number;
-}, 15000); // 15 секунд
+  await putStats()
+}, 60000); // 60 секунд
 
 const incrementButton2 = document.getElementById('feed');
 const numberDisplay2 = document.getElementById('hp-number');
@@ -84,10 +130,10 @@ incrementButton2.addEventListener('click', () => {
 
 //// TON wallet
 
-const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-  manifestUrl: 'https://nikishmyaps.github.io/tamagotchi/tonconnect-manifest.json',
-  buttonRootId: 'ton-connect'
-});
+// const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+//   manifestUrl: 'https://nikishmyaps.github.io/tamagotchi/tonconnect-manifest.json',
+//   buttonRootId: 'ton-connect'
+// });
 
 const client = new TonClient({
   endpoint: 'https://toncenter.com/api/v2/jsonRPC',
@@ -106,20 +152,22 @@ const getWalletBalance = async(walletAddress) => {
   }
 }
 
+let address = ''
 tonConnectUI.onStatusChange(info => {
   if( info ) {
+    localStorage.setItem('wallerAddress', info.account.address)
     getWalletBalance(info.account.address)
   }
   else document.getElementById('ton-balance').textContent = ''
 })
-
 document.getElementById('buy').addEventListener('click', async () => {
-  return await tonConnectUI.sendTransaction({messages: [
+  const res = await tonConnectUI.sendTransaction({messages: [
       {
         address: '0:e6a224fdc28dcba7e26e7a1b7d8ddfd7034b00eb746d4dc635241036f8b00e3b',
         amount: '100000000'
       }
     ]})
+  console.log('res', res)
 });
 
 document.getElementById('feed').addEventListener('click', async () => {
